e900fe36736b6d9fdf7d1ada98346951926d9f9f
Alberto Villegas <168059876+Gigoalb@users.noreply.github.com>
2026-01-21 23:44:25 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,4 +6,11 @@ app.get("/", (req, res) => {
   res.status(200).send("App Node.js en producción 🚀");
 });
 
+app.get("/health", (req, res) => {
+  res.status(200).json({
+    status: "ok",
+    uptime: process.uptime()
+  });
+});
+
 module.exports = app;
