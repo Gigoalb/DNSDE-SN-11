3b93ebc967e73812a073ec9449fe330b0c16b656
Alberto Villegas <168059876+Gigoalb@users.noreply.github.com>
2026-02-15 20:47:10 -0600

    Update index.js
    Update made on Endpoint

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 const express = require("express");
-
 const app = express();
 
 app.get("/", (req, res) => {
   res.status(200).send("App Node.js en producción 🚀");
 });
 
+// Endpoint mejorado para la monitorización
 app.get("/health", (req, res) => {
   res.status(200).json({
-    status: "ok",
-    uptime: process.uptime()
+    status: 'UP',               // Cambiado de "ok" a "UP" (estándar de industria)
+    timestamp: new Date().toISOString(), // Añadido formato ISO
+    uptime: process.uptime()    // Segundos que el proceso lleva activo
   });
 });
 
